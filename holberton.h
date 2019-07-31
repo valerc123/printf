7fc550765a52fce9321b3172f8a5464623e8c6f6
Nick Forero <nickforero@pop-os.localdomain>
2019-07-30 21:13:52 -0500

    Added the optimized header with pointer to function with struct as switch.

--- holberton.h
+++ holberton.h
@@ -1,9 +1,30 @@
 #ifndef _HOLBERTON_H_
 #define _HOLBERTON_H_
-
+#include <stdio.h>
+#include <unistd.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdarg.h>
 int _printf(const char *format, ...);
-int print_char(char c);
-int cases(va_list list, const char *format);
-int print_string(char *string);
 int _putchar(char c);
+int print_char(va_list parameters);
+int print_string(va_list parameters);
+int is_valid(char c);
+int print_integer(va_list parameters);
+int get_digits(unsigned int copy);
+int get_divider(int digits);
+int (*get_print_func(const char c))(va_list ap);
+/**
+ * struct format - Structure that saves the print function for each identifier.
+ *
+ * @type: char that represents the type of data that will be printed.
+ *
+ * @function: the pointer to the function that prints certain data type.
+ */
+struct format
+{
+	char type;
+	int (*function)(va_list);
+};
+typedef struct format format_t;
 #endif
