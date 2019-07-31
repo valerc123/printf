3f83bacaf237dc17365e64d145263c5a4ca7f995
Nick Forero <nickforero@pop-os.localdomain>
2019-07-30 21:28:26 -0500

    Updated the _printf function with the selector function, some optimizations and added the feature to print integers.

--- _printf.c
+++ _printf.c
@@ -1,21 +1,66 @@
-#include <stdio.h>
-#include <stdarg.h>
-#include <unistd.h>
 #include "holberton.h"
+
 /**
- * _printf - This function prints data according to a format
- * @format: type of data
- * Return: the integer written
+ * _printf - This function prints data according to a format string.
+ *
+ * @format: a string with the format of how the data will be printed.
+ *
+ * Return: an int with the numbers of bytes printed.
  */
+
 int _printf(const char *format, ...)
 {
-	int n = 0;
-	va_list list;
+	int count, i;
+	va_list parameters;
 
-	va_start(list, format);
-	if (!format)
+	if (format == NULL)
+	{
 		return (-1);
-	n = cases(list, format);
-	va_end(list);
-	return (n);
+	}
+	for (i = 0, count = 0, va_start(parameters, format); format[i] != '\0'; i++)
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			if (format[i] == '%')
+			{
+				count += _putchar('%');
+			}
+			else if (is_valid(format[i]))
+			{
+				count += get_print_func(format[i])(parameters);
+			}
+			else if (format[i] != '\0')
+			{
+				count += _putchar('%') + _putchar(format[i]);
+			}
+			else if (format[i] == '\0')
+			{
+				return (-1);
+			}
+			else
+			{
+				count += _putchar('%');
+			}
+		}
+		else
+		{
+			count += _putchar(format[i]);
+		}
+	}
+	va_end(parameters);
+	return (count);
+}
+
+/**
+ * is_valid - Checks if the format identifier char is valid.
+ *
+ * @c: the char with the format identifier that will be checked.
+ *
+ *Return: 1 if @ is a valid format identifier, else return 0.
+*/
+
+int is_valid(char c)
+{
+	return (c == 'c' || c == 's' || c == 'd' || c == 'i');
 }
